682d81d7cb892a1f4b64d944222c30315e5aa2c6
jclauneuro <jlau287@uwo.ca>
2015-09-28 15:55:38 -0400

    minor changes to createFStoSlicerTfm.m to better handle input from commandline

--- tools/matlab/createFStoSlicerTfm.m
+++ tools/matlab/createFStoSlicerTfm.m
@@ -1,7 +1,7 @@
 function createFStoSlicerTfm(in_orig_mgz,out_tfm);
 
-in_orig_mgz='/cluster/data/freesurfer/EPL14_LHS_0068/freesurfer/mri/orig.mgz';
-out_tfm='/tmp/test_final.tfm';
+%in_orig_mgz='/cluster/data/freesurfer/EPL14_LHS_0068/freesurfer/mri/orig.mgz';
+%out_tfm='/tmp/test_final.tfm';
 
 
 system(sprintf('mri_info --vox2ras-tkr %s > /tmp/Torig.txt',in_orig_mgz));
