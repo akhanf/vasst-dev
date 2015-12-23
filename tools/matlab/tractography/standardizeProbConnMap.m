60cdca4a8cf40b48c504c605fd3872d75828985a
Ali Khan <alik@robarts.ca>
2015-12-23 12:41:07 -0500

    fixed bug in conn std

--- tools/matlab/tractography/standardizeProbConnMap.m
+++ tools/matlab/tractography/standardizeProbConnMap.m
@@ -10,12 +10,27 @@ function  standardizeProbConnMap( in_nii,out_nii )
         vol(vol==0)=nan;
         [counts,centers]=hist(vol(:),nbins);
 
-        f=fit(centers',counts','power1');
+        f=fit(centers',counts','power1','Robust','LAR');
                 
         vol_scaled=nii.vol();
-        vol_scaled=(vol_scaled./f.a).^(-1/f.b);
+     %   vol_scaled=(vol_scaled./f.a).^(-1/f.b);
+       % vol_scaled=((vol_scaled).^(-1/f.b))./f.a;
+       %vol_scaled=(vol_scaled).^(-1/f.b);
+
+       vol_scaled=vol_scaled.^(-1/f.b).*(f.a^(1/f.b));
+       
+       %other:
+%        
+%        vol=vol_scaled;
+% vol_scaled(vol_scaled==0)=nan;
+% [counts_sc,centers_sc]=hist(vol_scaled(:),nbins);
+% figure; loglog(centers,counts);
+% 
+% 
+% 
+         nii.vol=vol_scaled;
         
-        nii.vol=vol_scaled;
+        
         
         save_nifti(nii,out_nii);
         
