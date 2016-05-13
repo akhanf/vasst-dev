fca4c61bda6a50cba131fc24db37432e4afa8141
Ali Khan <alik@robarts.ca>
2016-05-13 16:42:54 -0400

    "added code to save centroids, angles, areas, and eccentricities for each neuron"

--- tools/matlab/histology/generateFeatureMaps.m
+++ tools/matlab/histology/generateFeatureMaps.m
@@ -4,16 +4,25 @@ function  generateFeatureMaps(tif,res_microns,pad_microns)
 
 %default uses 100um+5um pad -- Maged's 20um used 50um pad -- 
 
+ref_res=100;
+
+
 
 if ~exist('res_microns')
     %resolution of output feature maps, in microns
-    res_microns=100;
+    res_microns=100; 
 end
 
 if ~exist('pad_microns')
     %padding of each chunk, in microns -- default is ~approx radius of neuron
-    pad_microns=5;
+   pad_microns=5;
+   %by default, use legacy naming (100um_FeatureMaps)
+   out_name=sprintf('%dum',res_microns);
+else
+    %otherwise, specify padding in name
+    out_name=sprintf('%dum_%dumPad',res_microns,pad_microns);
 end
+
 
 
 %tif='F:\Histology\EPI_P040\tif\EPI_P040_Neo_06_NEUN.tif';
@@ -40,13 +49,19 @@ lores=imread(lores_png);
 end
 
 
+
     
-outdir=sprintf('%s/../%dum_FeatureMaps',path,res_microns);
+outdir=sprintf('%s/../%s_FeatureMaps',path,out_name);
 mkdir(outdir);
 
 %end
 
 outmap=sprintf('%s/%s.mat',outdir,name);
+
+
+ref_map=sprintf('%s/../%dum_FeatureMaps/%s.mat',path,ref_res,name);
+
+
 
 %0.5um/pixel
 hist_res=0.5;
@@ -67,9 +82,9 @@ total_counts=zeros(size(xout));
 
 skip_sweep=false;
 %if feat map exists already, load it up and skip 1st sweep:
-if (exist(outmap))
+if (exist(ref_map))
     skip_sweep=true;
-    load(outmap);
+    load(ref_map,'threshold');
 end
 
 
@@ -83,7 +98,7 @@ if (~skip_sweep)
 
 %sweep 1:
 for i=1:Nx
-    i
+    
     for j=1:Ny
         
     
@@ -110,6 +125,8 @@ features={'count','area','perimeter','orientation','weighted_orientation','eccen
 
 featureVec=zeros(Nx,Ny,length(features));
 
+centroidVec=cell(Nx,Ny);
+
 
 %get features of big and small neurons too
 sizeThreshold=500;
@@ -129,20 +146,32 @@ padWidth=pad_microns./hist_res; %in pixels
 
 %roix=3:58;
 %roiy=81:211;
-
+ %roix=160:170;
+ %roiy=160:170;
 
 
 %now, have threshold, do second sweep to get pos pix frac
 for i=1:Nx
 %    for i=roix
- %   i
+ 
+% i
+disp(sprintf('%2.1f percent complete',double(i-1)./double(Nx)*100));
     for j=1:Ny
 %        for j=roiy
         
   %  j;
         [img]=getHiresChunkAperio(tif,Nx,Ny,i,j,0,padWidth); 
         
-        
+%         fh=figure; imagesc(img);
+%         c=centroidVec{i,j};
+%         hold on; scatter(c(:,1),c(:,2),'g.');
+%         set(gcf,'Position',[66 1 1855 1001]);
+%         axis equal;
+%         saveas(fh,sprintf('snapshot_%d_%d.jpg',i,j));
+%         
+%         close(fh);
+
+       
         stain_img=getStainChannel(img,stain_type);
         
         preWS=(threshold-stain_img)./255;         
@@ -156,7 +185,9 @@ for i=1:Nx
         %remove border 
         preWS=preWS.*imclearborder(preWS>0);
         
-        if(isempty(find(preWS>0)))
+       min_th=100; % number of pixels to be positive to perform watershed
+        
+        if(numel(find(preWS>0))<min_th)
             
             %nothing passes the preWS threshold, skip this chunk..
             continue
@@ -167,7 +198,7 @@ for i=1:Nx
     
         [wL] = watershed2(preWS,5); 
 
-        else if (strcmp(strct,'Hp'));
+        elseif (strcmp(strct,'Hp'));
             
         [wL] = watershed2(preWS,6); 
 
@@ -210,7 +241,10 @@ for i=1:Nx
             featureVec(i,j,6)=mean(eccen);
             featureVec(i,j,7)=mean(pdist(centroid));
             
-            
+            centroidVec(i,j)={centroid};
+            angleVec(i,j)={angle};
+            areaVec(i,j)={area};
+            eccenVec(i,j)={eccen};
             % fill in featureVec_sml and featureVec_big
             % with features_bysize
             
@@ -274,7 +308,7 @@ for i=1:Nx
  %end
 
 
-save(outmap,'featureVec','features','total_counts','xout','threshold','featureVec_sml','featureVec_big','features_bysize');
+save(outmap,'featureVec','features','total_counts','xout','threshold','featureVec_sml','featureVec_big','features_bysize','centroidVec','angleVec','areaVec','eccenVec','pad_microns','res_microns','ref_map');
 
 
 %end %exist outmap
