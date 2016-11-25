c8ea4864e85d12d2234c4ef5dd6dd77472ab5d80
Ali Khan <alik@robarts.ca>
2016-11-25 16:03:05 -0500

    added exports

--- init_vasst_dev.sh
+++ init_vasst_dev.sh
@@ -26,12 +26,13 @@ then
   export VASST_DEV_HOME=/cluster/software/vasst-dev
   echo "Defining default VASST_DEV_HOME, $VASST_DEV_HOME" 
 else
+  export VASST_DEV_HOME
  echo "VASST_DEV_HOME already defined as $VASST_DEV_HOME"
 fi
 
 
-PIPELINE_DIR=$VASST_DEV_HOME/pipeline
-PIPELINE_TOOL_DIR=$VASST_DEV_HOME/tools
+export PIPELINE_DIR=$VASST_DEV_HOME/pipeline
+export PIPELINE_TOOL_DIR=$VASST_DEV_HOME/tools
 
 export PIPELINE_CFG_DIR=$PIPELINE_DIR/cfg
 export PATH="$PIPELINE_DIR:$PIPELINE_DIR/fmri:$PIPELINE_DIR/diffusion:$PIPELINE_DIR/registration:$PIPELINE_DIR/t1:$PIPELINE_DIR/batch:$PIPELINE_DIR/import:$PIPELINE_DIR/qc:$PIPELINE_TOOL_DIR:$PATH"
