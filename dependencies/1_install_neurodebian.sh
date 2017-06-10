9eef531b95e5b23fe7f0c6f0891a736ab484c163
akhanf <alik@robarts.ca>
2017-06-10 19:08:20 -0400

    added neurodeb

--- dependencies/1_install_neurodebian.sh
+++ dependencies/1_install_neurodebian.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+wget -O- http://neuro.debian.net/lists/xenial.us-nh.full | tee /etc/apt/sources.list.d/neurodebian.sources.list
+apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
 
-echo placeholder for this!
