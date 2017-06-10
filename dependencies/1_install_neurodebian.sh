500a31b0d9eae4aeb7d07192cc3d9fb8781d5bb2
akhanf <alik@robarts.ca>
2017-06-10 19:56:45 -0400

    added key for neurodebian

--- dependencies/1_install_neurodebian.sh
+++ dependencies/1_install_neurodebian.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-wget -O- http://neuro.debian.net/lists/xenial.us-nh.full | tee /etc/apt/sources.list.d/neurodebian.sources.list
-apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
+in_key=`dirname $0`/../keys/neurodebian.gpg
+
+apt-get install -y --no-install-recommends curl bzip2 ca-certificates xvfb && \
+    curl -sSL http://neuro.debian.net/lists/xenial.us-ca.full >> /etc/apt/sources.list.d/neurodebian.sources.list && \
+    apt-key add $in_key && \
+    (apt-key adv --refresh-keys --keyserver hkp://ha.pool.sks-keyservers.net 0xA5D32F012649A5A9 || true) && \
+    apt-get update
+
+
 
