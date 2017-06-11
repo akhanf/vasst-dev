53332962a86b265f12c573d52e40baef8686bf12
akhanf <alik@robarts.ca>
2017-06-11 00:37:10 -0400

    added back key retrieval

--- dependencies/1_install_neurodebian.sh
+++ dependencies/1_install_neurodebian.sh
@@ -5,7 +5,7 @@ in_key=`dirname $0`/../keys/neurodebian.gpg
 apt-get install -y --no-install-recommends curl bzip2 ca-certificates xvfb && \
     curl -sSL http://neuro.debian.net/lists/xenial.us-ca.full >> /etc/apt/sources.list.d/neurodebian.sources.list && \
     apt-key add $in_key && \
-#    (apt-key adv --refresh-keys --keyserver hkp://ha.pool.sks-keyservers.net 0xA5D32F012649A5A9 || true) && \
+    (apt-key adv --refresh-keys --keyserver hkp://ha.pool.sks-keyservers.net 0xA5D32F012649A5A9 || true) && \
     apt-get update
 
 
