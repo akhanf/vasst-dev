0cdda76fe6a5f81b1653ad253bf2dcf1ee7db9e5
akhanf <alik@robarts.ca>
2017-07-17 22:12:16 -0400

    missing deps

---
+++ tools/matlab/readTriByu.m
@@ -0,0 +1,18 @@
+function [faces, vertices] = readTriByu(filename,fixedwidth)
+% [faces, vertices] = readTriBYU(filename,fixedwidth)
+% Reads a BYU file if it holds a triangulated mesh
+%
+% filename - path to BYU file
+% fixedwidth - 0 or 1 refers to which style of byu file (whether numbers are spaced into fixed width columns)
+% vertices - vertices of the BYU file
+% edges - vector of indices into the vertex array. polygons are made from 
+%       consecutive edges, with the final edge of each polygon being negated.
+%       i.e. [1;4;3;-5;1;2;-3;1;5;-3] would describe a quadrangle and 2
+%       triangless
+% parts - vector delineating which edges belong in which parts
+filename = enscPathAsLocal(filename);
+if nargin<2
+    fixedwidth = 0;
+end
+[parts,vertices,edges] =readBYUSurface(filename,fixedwidth);
+faces = abs(reshape(edges,3,[])');
---
+++ tools/matlab/writeTriByu.m
@@ -0,0 +1,18 @@
+function writeTriByu(filename, triangles, vertices)
+% writeTriByu(filename, triangles, vertices)
+% Writes a triangular mesh to BYU format
+% filename - path to BYU file to create
+% vertices - Mx3 matrix of vertex coordinates
+% triangles - Mx3 matrix of indices into the vertices matrix
+filename = enscPathAsLocal(filename);
+dlmwrite(filename, [1 size(vertices,1) size(triangles,1) size(triangles,1)*3], 'delimiter', ' ', 'precision', '%i') ;
+
+% $$$ dlmwrite(filename, [1 size(triangles,1)*3], '-append', 'delimiter', ' ', 'precision', '%i') ;
+
+%EDIT: Karteek Popuri (karteek.pop@gmail.com) on Jan 22, 2014.
+%In the .byu format the second line should be the number of triangles as opposed to 3*number of triangles. 
+%So, line 12 is commented out and has been replaced by line 17.
+
+dlmwrite(filename, [1 size(triangles,1)], '-append', 'delimiter', ' ', 'precision', '%i') ;
+dlmwrite(filename, vertices, '-append', 'delimiter', ' ', 'precision', '%f') ;
+dlmwrite(filename, [triangles(:,1) triangles(:,2) triangles(:,3)], '-append', 'delimiter', ' ', 'precision', '%i') ;
