89cad441d0a5316b4aa3dc0a71518a44094cf5b5
akhanf <alik@robarts.ca>
2018-02-04 21:38:08 -0500

    bids-compliant DESPOT1 processing

--- tools/getValueJson.py
+++ tools/getValueJson.py
@@ -7,17 +7,13 @@ import json
 
 
 if len(sys.argv) < 3:
-	print('Usage: '+sys.argv[0]+' <in_json> <key name> <convert sec to ms (default: 0)>')
+	print('Usage: '+sys.argv[0]+' <in_json> <key name> <optional keys if nested ...>')
 	exit(1)
  
 
 in_file=sys.argv[1]
 key=sys.argv[2]
 
-do_sec_to_ms=0
-
-if len(sys.argv) >3:
-	do_sec_to_ms=int(sys.argv[3])
 
 
 
@@ -25,8 +21,8 @@ if len(sys.argv) >3:
 with open(in_file) as data_file:
 	data = json.load(data_file)
 	val=data[key]
-	if do_sec_to_ms == 1:
-		val=val*1000
+	for i in range(3,len(sys.argv)):
+		key=sys.argv[i]
+		val=val[key]
+
 	print(val)
-
-
