12655864487e39b9aa3d9af16de22e6b2f2985e0
akhanf <alik@robarts.ca>
2018-03-02 10:02:16 -0500

    added standard qspace sampling tables from Dec 2016

---
+++ tools/matlab/qspaceSampling/genSiemensGradTables.m
@@ -0,0 +1,3 @@
+%generates Siemens gradient tables
+convertOptimalGradTableToSiemens('multishell_90_dist2.txt',6,'dwi96_multishell_6+30+60.dvs')
+convertOptimalGradTableToSiemens('singleshell_63.txt',6,'dwi69_singleshell_6+63.dvs');
